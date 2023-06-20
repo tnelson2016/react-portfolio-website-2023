6fa7506d74b9f459a78070bc80d9ba0d382e3c77
tnelson2016 <troy.nelson@bracari.com>
2023-06-19 23:11:48 -0400

    fixed bugs part 1

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,14 +10,14 @@ const Footer = () => {
 <ul className='permalinks'>
       <li><a href="#">Home</a></li>
       <li><a href="#about">About</a></li>
-      <li><a href="#Experience">Experience</a></li>
-      <li><a href="#Portfolio">Portfolio</a></li>
-      <li><a href="#Contact">Contact</a></li>
+      <li><a href="#experience">Experience</a></li>
+      <li><a href="#portfolio">Portfolio</a></li>
+      <li><a href="#contact">Contact</a></li>
 </ul>
 
       <div className="footer__socials">
-          <a href="https://linkedin.com" target="_blank"><BsLinkedin className= 'footer__logo' target='_blank'/></a>
-          <a href="https://github.com" target="_blank"><FaGithub className= 'footer__logo' target='_blank' /></a>
+          <a href="https://www.linkedin.com/in/troy-l-nelson-/" target="_blank"><BsLinkedin className= 'footer__logo' target='_blank'/></a>
+          <a href="https://github.com/tnelson2016" target="_blank"><FaGithub className= 'footer__logo' target='_blank' /></a>
       </div>   
       <div className="footer__copyright">
         <small>Developed and Designed by Troy Nelson</small>
